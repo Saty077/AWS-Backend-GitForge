5791b79dd8e548fd20bf1a6545cb04d04dd92625
Satyam Jha <satyamjha2743@gmil.com>
2025-12-13 21:24:50 +0530

    feat: implement initRepo logic

--- controllers/init.js
+++ controllers/init.js
@@ -1,3 +1,20 @@
+import fs from "fs/promises";
+import path from "path";
+
 export const initRepo = async () => {
-  console.log("Calling init command");
+  const repoPath = path.resolve(process.cwd(), ".gitTracker");
+  const commitPath = path.join(repoPath, "commits");
+
+  try {
+    await fs.mkdir(repoPath, { recursive: true });
+    await fs.mkdir(commitPath, { recursive: true });
+    await fs.writeFile(
+      path.join(repoPath, "config.json"),
+      JSON.stringify({ BUCKET: process.env.S3_BUCKET })
+    );
+
+    console.log("Repository initialized!");
+  } catch (err) {
+    console.log("Something went wrong while initializing repo", err);
+  }
 };
