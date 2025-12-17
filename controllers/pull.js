d4aa97631262430298ce959d035154ba0acb4c76
Satyam Jha <satyamjha2743@gmil.com>
2025-12-17 22:26:00 +0530

    feat: Implement pull commnad

--- controllers/pull.js
+++ controllers/pull.js
@@ -1,3 +1,41 @@
+import fs from "fs/promises";
+import path from "path";
+import { s3, S3_BUCKET } from "../config/aws_config.js";
+
 export const pullRepo = async () => {
-  console.log("Calling pullRepo");
+  const repoPath = path.join(process.cwd(), ".gitForge");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    const data = await s3
+      .listObjectsV2({
+        Bucket: S3_BUCKET,
+        Prefix: "commits/",
+      })
+      .promise();
+
+    const objects = await data.Contents;
+
+    for (const object of objects) {
+      const key = object.Key;
+      const commitDir = path.join(
+        commitsPath,
+        path.dirname(key).split("/").pop()
+      );
+
+      await fs.mkdir(commitDir, { recursive: true });
+
+      const params = {
+        Bucket: S3_BUCKET,
+        Key: key,
+      };
+
+      const fileContent = await s3.getObject(params).promise();
+
+      await fs.writeFile(path.join(repoPath, key), fileContent.Body);
+      console.log("All file are from S3");
+    }
+  } catch (err) {
+    console.error("Error connecting to pullRpo", err);
+  }
 };
