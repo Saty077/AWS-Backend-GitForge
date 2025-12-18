6e1670b92f6f52f061bff93d9cc05ea231e77e61
Satyam Jha <satyamjha2743@gmail.com>
2025-12-18 09:45:02 +0530

    feat: Implement revert command

--- controllers/revert.js
+++ controllers/revert.js
@@ -1,3 +1,23 @@
-export const revertRepo = async () => {
-  console.log("Calling revertRepo");
+import fs from "fs";
+import path from "path";
+import { promisify } from "util";
+
+const readdir = promisify(fs.readdir);
+const copyfile = promisify(fs.copyFile);
+
+export const revertRepo = async (commitId) => {
+  const repoPath = path.join(process.cwd(), ".gitForge");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    const commitsDir = path.join(commitsPath, commitId);
+    const files = await readdir(commitsDir);
+    const parentDir = path.resolve(repoPath, "..");
+    for (const file of files) {
+      await copyfile(path.join(commitsDir, file), path.join(parentDir, file));
+    }
+    console.log(`Successfully reverted to commit ${commitId}`);
+  } catch (err) {
+    console.error("Error occured in reverting files", err);
+  }
 };
