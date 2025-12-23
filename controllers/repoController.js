96a164b469218626df740594ac7f50e9d4c51cae
Satyam Jha <satyamjha2743@gmil.com>
2025-12-23 18:56:49 +0530

    feat: work on createRepository functionality

--- controllers/repoController.js
+++ controllers/repoController.js
@@ -1,5 +1,39 @@
+import mongoose from "mongoose";
+import httpStatus from "http-status";
+import Repository from "../models/repoModel.js";
+import User from "../models/userModel.js";
+import Issue from "../models/issueModel.js";
+
 export const createRepository = async (req, res) => {
-  res.send("createRepository called");
+  const { owner, name, description, content, visibility, issues } = req.body;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(owner))
+      return res.status(httpStatus.BAD_REQUEST).json("Invalid userid!");
+
+    if (!name) return res.status(httpStatus.BAD_REQUEST).json("Name required!");
+
+    const newRepo = new Repository({
+      owner,
+      name,
+      description,
+      content,
+      visibility,
+      issues,
+    });
+
+    const result = await newRepo.save();
+    const repoId = result._id.toString();
+
+    res
+      .status(httpStatus.CREATED)
+      .json({ message: "repo Created!", RepositoryID: repoId });
+  } catch (err) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json(err.message);
+    console.error(
+      "Something went wrong while creating Repository",
+      err.message
+    );
+  }
 };
 export const getAllRepositories = async (req, res) => {
   res.send("getAllRepositories called");
