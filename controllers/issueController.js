672c7a5160c68c6b77ce9b93f32b6a366f5cf087
Satyam Jha <satyamjha2743@gmil.com>
2025-12-24 17:08:59 +0530

    feat: Work on createIssue functionality

--- controllers/issueController.js
+++ controllers/issueController.js
@@ -1,6 +1,34 @@
+import mongoose from "mongoose";
+import httpStatus from "http-status";
+import Repository from "../models/repoModel.js";
+import User from "../models/userModel.js";
+import Issue from "../models/issueModel.js";
+
 export const createIssue = async (req, res) => {
-  res.send("createIssue Called");
+  const { title, description } = req.body;
+  const { id } = req.params;
+
+  try {
+    if (title || description || id)
+      return res
+        .status(httpStatus.BAD_REQUEST)
+        .json({ message: "Missing required fields!" });
+
+    const newIssue = new Issue({
+      title,
+      description,
+      repository: id,
+    });
+
+    await newIssue.save();
+
+    res.status(httpStatus.CREATED).json({ message: "Issue Created", newIssue });
+  } catch (err) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json(err.message);
+    console.error("Something went wrong while creating Issue", err.message);
+  }
 };
+
 export const updateIssueById = async (req, res) => {
   res.send("updateIssueById Called");
 };
